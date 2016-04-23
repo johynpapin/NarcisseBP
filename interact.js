1cc77914397ab728eef74b2f7154f8dc78538f12
Papin Johyn <johynpapin@kiwiasso.org>
2016-04-23 12:18:42 +0200

    Initial commit

---
+++ interact.js
@@ -0,0 +1,40 @@
+export function log(data, out = 'console') {
+    switch(out) {
+        case 'chat':
+            channel.socket.emit("chatMessage", data);
+            break;
+        case 'console':
+            console.log(data);
+            break;
+    }
+};
+export function warn(data, out = 'console') {
+    switch(out) {
+        case 'chat':
+            channel.socket.emit("chatMessage", "Warning : "+msg);
+            break;
+        case 'console':
+            console.warn(data);
+            break;
+    }
+};
+export function info(data, out = 'console') {
+    switch(out) {
+        case 'chat':
+            channel.socket.emit("chatMessage", "Info : "+msg);
+            break;
+        case 'console':
+            console.info(data);
+            break;
+    }
+};
+export function error(data, out = 'console') {
+    switch(out) {
+        case 'chat':
+            channel.socket.emit("chatMessage", "Error : "+msg);
+            break;
+        case 'console':
+            console.error(data);
+            break;
+    }
+};
---
+++ narcisse.js
@@ -0,0 +1,1 @@
+import * as interact from 'interact';
